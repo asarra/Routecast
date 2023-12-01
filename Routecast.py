20d2b14eeea731ca68cde732e38f72b08d8e42e9
asarra <20255127+asarra@users.noreply.github.com>
2023-12-01 12:34:09 +0100

    Benutzeroberfläche funktioniert halbwegs

--- Routecast.py
+++ Routecast.py
@@ -1,19 +1,49 @@
 from methods import *
 import place
+from flask import Flask, request, render_template
 
 
-place1 = place.Place()
-place2 = place.Place()
+def createDetailedMap(placeTerm1, placeTerm2):
+    place1 = place.Place()
+    place2 = place.Place()
 
-placeTerm = "wortmann hüllhors"
-place1.x, place1.y, place1.name = getXYName(placeTerm)
+    # text = "wortmann hüllhors"
+    place1.x, place1.y, place1.name = getXYName(placeTerm1)
 
-placeTerm = "Lübbecke Berufskoll"
-place2.x, place2.y, place2.name = getXYName(placeTerm)
+    # text2 = "Lübbecke Berufskoll"
+    place2.x, place2.y, place2.name = getXYName(placeTerm2)
 
-place1.clouds, place1.temp = getWeather(place1.x, place1.y)
-place2.clouds, place2.temp = getWeather(place2.x, place2.y)
+    place1.clouds, place1.temp = getWeather(place1.x, place1.y)
+    place2.clouds, place2.temp = getWeather(place2.x, place2.y)
 
-distance, duration, steps, polygon = getDirection(place1.x, place1.y, place2.x, place2.y)
+    distance, duration, steps, polygon = getDirection(place1.x, place1.y, place2.x, place2.y)
 
-createMap(place1, place2, distance, duration, steps, polygon)
+    createMap(place1, place2, distance, duration, steps, polygon)
+
+
+def openDetailedMap():
+    import os
+    os.system("start index.html")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def detailedMap():
+    return app.send_static_file('.\index.html')
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def form_post():
+    placeTerm1 = request.form['text']
+    placeTerm2 = request.form['text2']
+    createDetailedMap(placeTerm1, placeTerm2)
+    # openDetailedMap()
+    # return app.send_static_file('.\index.html')
+    ##  return form()
+    return detailedMap()
+
+app.run(host='127.0.0.1', port=5000)
